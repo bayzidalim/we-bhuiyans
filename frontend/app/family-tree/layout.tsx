0df91c9519b040d23deef2b665ae4b2d97b36ae5
Bayzid Alim <bayzidalim@gmail.com>
2025-12-18 19:03:51 +0600

    Fixed bugs

--- frontend/app/family-tree/layout.tsx
+++ frontend/app/family-tree/layout.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import AuthGuard from '@/app/components/AuthGuard';
+
 
 export default function FamilyTreeLayout({ children }: { children: React.ReactNode }) {
   return (
-    <AuthGuard>
+    <>
       {children}
-    </AuthGuard>
+    </>
   );
 }
